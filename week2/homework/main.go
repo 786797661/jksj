6a26b33ff5f926da1a887a3aca1fba51e7142dce
e <786797661@qq.com>
2022-01-28 16:13:49 +0800

    homework

--- week2/homework/main.go
+++ week2/homework/main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	pkgerrors "github.com/pkg/errors"
 	"strings"
 )
 
@@ -20,37 +19,41 @@ const (
 	dbName   = "MySql002"
 )
 
-func main() {
-	//构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=utf8"
-	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
-	//打开数据库,前者是驱动名，所以要导入： _ "github.com/go-sql-driver/mysql"
-	database, err := sql.Open("mysql", path)
-	//database, err := sqlx.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		fmt.Println("open mysql failed,", err)
-		return
-	}
-	Db = database
-	err = Query("select username from scholar_userinfo where id =?", Db)
-	fmt.Printf("original error : %T %v\n", pkgerrors.Cause(err), pkgerrors.Cause(err))
-	defer Db.Close() // 注意这行代码要写在上面err判断的下面
-}
-
-func Query(sqlStr string, Db *sql.DB) error {
-	err := sqlFacade(sqlStr, 1, Db)
-	return err
-}
-
 type sqlError struct {
 	sqlStr string
 	err    error
 	res    interface{}
 }
 
-func (e *sqlError) UnWrap() error {
+func main() {
+	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	database, err := sql.Open("mysql", path)
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = database
+	err = Query("select username from userinfo where id =?", 1, Db)
+	te, _ := err.(*sqlError)
+	if te.err != nil {
+		//处理除了sql.ErrNoRows的错误
+	} else if te.res == nil {
+		//处理没有数据的情况
+	} else if te.res != nil {
+		//处理正常数据
+	}
+	defer Db.Close()
+}
 
+func Query(sqlStr string, args interface{}, Db *sql.DB) error {
+	err := sqlFacade(sqlStr, args, Db)
+	return err
+}
+
+func (e *sqlError) UnWrap() error {
 	return e.err
 }
+
 func (e *sqlError) Error() string {
 	return e.err.Error()
 }
@@ -58,6 +61,7 @@ func (e *sqlError) Error() string {
 func sqlFacade(sqlStr string, args interface{}, db *sql.DB) error {
 	var res interface{}
 	err := db.QueryRow(sqlStr, args).Scan(&res)
+	//如果是sql.ErrNoRows 降级返回nil
 	if errors.Is(err, sql.ErrNoRows) {
 		err = nil
 	}
